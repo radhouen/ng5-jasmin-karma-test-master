a89a6e314f63a1120a8d76c4aa71f36e9f859231
assakra radhouen <askriradhouen09@gmail.com>
2018-04-17 07:30:53 +0100

    Create model proglanguages
    id,title,description and icon columns

---
+++ angularTddTest/src/app/models/proglanguages.ts
@@ -0,0 +1,8 @@
+export class Proglanguages {
+	constructor(
+        public id: number,
+        public title: string,
+        public icon: string,
+        public description: string,
+      ) {  }
+}
